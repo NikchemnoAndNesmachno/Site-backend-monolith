57362469df9661694e1cfce881f84e0b71dca09e
LainIsHDEATH <ivan.shankin04@gmail.com>
2026-01-31 17:33:38 +0200

    Added VideoService with methods: - uploadVideoWithPreview that uses mediaService methods to create bundle, then create Video object with status "UPLOADING", save original video file and preview on disk as assets, add created media aseets as bundle items to bundle, set Video status as "READY". Video belongs to a user, that called that method. - deleteVideoWithPreview for deletion user's video from DB (only owner can delete theirs video). Deletion also deletes media bundle and media items that belong to that video (but not media assets, as it may be used in other places).
    Added VideoController with endpoints:
    - /upload/video-with-preview to upload media files and metadata for adding user's video.
    - /{id} delete to delete user's video and preview.

---
+++ media/src/main/java/ua/nin/media/controller/VideoController.java
@@ -0,0 +1,40 @@
+package ua.nin.media.controller;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import ua.nin.media.dto.VideoWithPreviewUploadResponse;
+import ua.nin.media.model.VideoVisibility;
+import ua.nin.media.service.VideoService;
+
+@RestController
+@RequestMapping("/api/v1/video")
+@RequiredArgsConstructor
+public class VideoController {
+
+    private final VideoService videoService;
+
+    @PostMapping(value = "/upload/video-with-preview", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<VideoWithPreviewUploadResponse> uploadVideoWithPreview(
+            @NotNull @RequestPart("title") String title,
+            @RequestPart(value = "description", required = false) String description,
+            @RequestPart(value = "visibility", required = false) VideoVisibility visibility,
+            @RequestPart("video") MultipartFile video,
+            @RequestPart("preview") MultipartFile preview,
+            Authentication authentication
+    ) {
+        long userId = Long.parseLong(authentication.getName());
+        return ResponseEntity.ok(videoService.uploadVideoWithPreview(title, description, visibility, userId, video, preview));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable long id, Authentication authentication) {
+        long userId = Long.parseLong(authentication.getName());
+        videoService.deleteVideoWithPreview(userId, id);
+        return ResponseEntity.noContent().build();
+    }
+}
