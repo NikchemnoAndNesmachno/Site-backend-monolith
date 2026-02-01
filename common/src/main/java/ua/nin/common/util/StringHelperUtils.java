2176f2e8975286f2b4a237befbc273cb97bb6c79
LainIsHDEATH <ivan.shankin04@gmail.com>
2026-02-01 08:23:55 +0200

    StringHelperUtils

--- common/src/main/java/ua/nin/common/util/StringHelperUtils.java
+++ common/src/main/java/ua/nin/common/util/StringHelperUtils.java
@@ -3,8 +3,7 @@ package ua.nin.common.util;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import static org.apache.commons.lang3.StringUtils.toRootLowerCase;
-import static org.apache.commons.lang3.StringUtils.trimToNull;
+import static org.apache.commons.lang3.StringUtils.*;
 
 @Component
 @NoArgsConstructor
@@ -29,5 +28,15 @@ public final class StringHelperUtils {
         String t = trimToNull(trimToNull(s));
         if (t == null) return null;
         return t.length() <= maxLength ? t : t.substring(0, maxLength);
+    }
+
+    public static String normalizeTargetType(String s) {
+        String t = trimToNull(trimToNull(s));
+        return toRootUpperCase(t);
+    }
+
+    public static String normalizeReactionCode(String s) {
+        String t = trimToNull(trimToNull(s));
+        return toRootUpperCase(t);
     }
 }
