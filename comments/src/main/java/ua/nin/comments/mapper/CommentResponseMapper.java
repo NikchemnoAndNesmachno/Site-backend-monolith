ab676ae05724d91b1093faafa481bc608a46dc0a
LainIsHDEATH <ivan.shankin04@gmail.com>
2026-02-03 04:47:48 +0200

    - Fixed small CommentResponseMapper bug for toDtoPublic method.

--- comments/src/main/java/ua/nin/comments/mapper/CommentResponseMapper.java
+++ comments/src/main/java/ua/nin/comments/mapper/CommentResponseMapper.java
@@ -2,20 +2,16 @@ package ua.nin.comments.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Named;
 import ua.nin.comments.dto.CommentResponse;
 import ua.nin.comments.model.Comment;
-import ua.nin.comments.model.CommentStatus;
 
 @Mapper(componentModel = "spring")
 public interface CommentResponseMapper {
     CommentResponse toDto(Comment comment);
 
-    @Mapping(target = "body", qualifiedByName = "mapPublicBody")
+    @Mapping(
+            target = "body",
+            expression = "java(comment.getStatus() == CommentStatus.ACTIVE ? comment.getBody() : \"[hidden]\")"
+    )
     CommentResponse toDtoPublic(Comment comment);
-
-    @Named("mapPublicBody")
-    default String mapPublicBody(Comment comment) {
-        return (comment.getStatus() == CommentStatus.ACTIVE) ? comment.getBody() : "[hidden]";
-    }
 }
