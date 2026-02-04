9494a6b8cc9d7369a3346930582e06be77415bee
LainIsHDEATH <ivan.shankin04@gmail.com>
2026-02-04 19:19:53 +0000

    Deploying to gh-pages from @ NikchemnoAndNesmachno/Site-backend-monolith@56ec22115e9d5d10777ae4c103c7037bde12a15c 🚀

--- common/src/main/java/ua/nin/common/util/StringHelperUtils.java
+++ common/src/main/java/ua/nin/common/util/StringHelperUtils.java
@@ -1,12 +1,13 @@
 package ua.nin.common.util;
 
+import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import static org.apache.commons.lang3.StringUtils.*;
 
 @Component
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class StringHelperUtils {
 
     public static String normalizeEmail(String email) {
